9ec829b1e449a416394fabc90c6acc6294ea80d3
szilard <szilard@users.noreply.github.com>
2016-11-03 13:25:55 -0700

    xgb

--- run-master.sh
+++ run-master.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-time ./run.sh r3 h2o | tee res-r3-h2o.csv
-time ./run.sh x1 h2o | tee res-x1-h2o.csv
+time ./run.sh r3 h2o     | tee res-r3-h2o.csv
+time ./run.sh x1 h2o     | tee res-x1-h2o.csv
 time ./run.sh r3 xgboost | tee res-r3-xgboost.csv
 time ./run.sh x1 xgboost | tee res-x1-xgboost.csv
+cat res-*.csv > res.csv
+
+#time ./run.sh r3 h2o     | tee    res.csv
+#time ./run.sh x1 h2o     | tee -a res.csv
+#time ./run.sh r3 xgboost | tee -a res.csv
+#time ./run.sh x1 xgboost | tee -a res.csv
+
+
+
 
