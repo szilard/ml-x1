cdc0d5edf0bb504483754d71027318451dff8fa6
szilard <szilard@users.noreply.github.com>
2016-11-04 11:27:14 -0700

    new cpucores format

--- run-master.sh
+++ run-master.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # on r3:
-time ./run.sh r3 h2o     | tee res-r3-h2o.csv
+time ./run.sh r3 h2o | tee res-r3-h2o.csv
 time ./run.sh r3 xgb | tee res-r3-xgb.csv
 # on x1:
-time ./run.sh x1 h2o     | tee res-x1-h2o.csv
+time ./run.sh x1 h2o | tee res-x1-h2o.csv
 time ./run.sh x1 xgb | tee res-x1-xgb.csv
 # then:
 cat res-*.csv > res.csv
